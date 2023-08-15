6c1d8ea3eddfd41b87e71d6cf5a577fb295ab60d
minh23122 <ducminhhungdao@gmail.com>
2023-08-15 14:07:06 +0700

    update

--- 04. Developement/KITS2023_Backend_B4/src/main/java/b4/kits2023_backend_b4/repository/UserRepository.java
+++ 04. Developement/KITS2023_Backend_B4/src/main/java/b4/kits2023_backend_b4/repository/UserRepository.java
@@ -3,5 +3,9 @@ package b4.kits2023_backend_b4.repository;
 import b4.kits2023_backend_b4.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User,Integer> {
+    public Optional<User> findByUsername(String username);
+    public Optional<User> findByEmail(String email);
 }
