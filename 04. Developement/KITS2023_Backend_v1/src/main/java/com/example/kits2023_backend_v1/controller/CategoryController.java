e6121d1d39678d1976aa0089e580977b28dc6950
minh23122 <ducminhhungdao@gmail.com>
2023-08-16 16:28:57 +0700

    update be and report

---
+++ 04. Developement/KITS2023_Backend_v1/src/main/java/com/example/kits2023_backend_v1/controller/CategoryController.java
@@ -0,0 +1,33 @@
+package com.example.kits2023_backend_v1.controller;
+
+import com.example.kits2023_backend_v1.model.Category;
+import com.example.kits2023_backend_v1.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/category")
+public class CategoryController {
+    @Autowired
+    private CategoryRepository categoryRepository;
+    @GetMapping("/all")
+    public List<Category> getAllCategory(){
+        return categoryRepository.findAll();
+    }
+    @PostMapping
+    public Category insertCategory(@RequestBody Category category){
+        return categoryRepository.save(category);
+    }
+    @DeleteMapping("{id}")
+    public void deleteCategory(@PathVariable int id){
+        categoryRepository.deleteById(id);
+    }
+    @PutMapping("{id}")
+    public Category updateCategory(@PathVariable int id, @RequestBody Category category){
+        Category updateCategory=categoryRepository.findById(id).get();
+        updateCategory.setName(category.getName());
+        return categoryRepository.save(updateCategory);
+    }
+}
